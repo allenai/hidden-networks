b873d6202fd985fe4259eec92d01a07ec881cb81
Vivek Ramanujan <vivekr@allenai.org>
2019-11-19 11:36:15 -0800

    Added a README

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 from models.resnet import ResNet18, ResNet50, ResNet101, WideResNet50_2, WideResNet101_2
 from models.resnet_cifar import cResNet18, cResNet50, cResNet101
 from models.frankle import FC, Conv2, Conv4, Conv6, Conv4Wide, Conv8, Conv6Wide
-from models.vgg import VGG13, VGG16, VGG19, VGG11
 
 __all__ = [
     "ResNet18",
@@ -18,8 +17,4 @@ __all__ = [
     "Conv4Wide",
     "Conv8",
     "Conv6Wide",
-    "VGG11",
-    "VGG13",
-    "VGG16",
-    "VGG19",
 ]
